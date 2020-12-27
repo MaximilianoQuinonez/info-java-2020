c6c2daa8ed20dc440a99673fcc544f0490275250
Maximiliano Quinonez <quinonezmaximiliano@gmail.com>
2020-12-26 22:01:43 -0300

    Versión final

--- src/main/java/com/informatorio/empmanager/entity/Post.java
+++ src/main/java/com/informatorio/empmanager/entity/Post.java
@@ -19,10 +19,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity 
 public class Post implements Serializable{
 
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/informatorio/empmanager/entity/Usuario.java
+++ src/main/java/com/informatorio/empmanager/entity/Usuario.java
@@ -15,10 +15,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity 
 public class Usuario implements Serializable {
 
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
